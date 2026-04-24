38ff8949b3f862771c6588e2b896087c35a2d90b
Prathyusha VK-0003 <prathyusha@sskatt.com>
2026-04-24 16:11:01 +0530

    updated changes in Add to cart

--- Shop.js
+++ Shop.js
@@ -1,165 +1,4 @@
-const products = [
-  {
-    id: 1,
-    name: "Syltherine",
-    subtitle: "Stylish cafe chair",
-    price: 2500000,
-    oldPrice: 3500000,
-    badgeType: "sale",
-    badgeText: "-30%",
-    image: "../assets/Syltherine.png"
-  },
-  {
-    id: 2,
-    name: "Leviosa",
-    subtitle: "Stylish cafe chair",
-    price: 2500000,
-    oldPrice: null,
-    badgeType: null,
-    badgeText: "",
-    image: "../assets/Leviosa.png"
-  },
-  {
-    id: 3,
-    name: "Lolito",
-    subtitle: "Luxury big sofa",
-    price: 7000000,
-    oldPrice: 14000000,
-    badgeType: "sale",
-    badgeText: "-50%",
-    image: "../assets/Lolito.png"
-  },
-  {
-    id: 4,
-    name: "Respira",
-    subtitle: "Outdoor bar table and stool",
-    price: 500000,
-    oldPrice: null,
-    badgeType: "new",
-    badgeText: "New",
-    image: "../assets/Respira.jpg"
-  },
-  {
-    id: 5,
-    name: "Syltherine",
-    subtitle: "Stylish cafe chair",
-    price: 2500000,
-    oldPrice: 3500000,
-    badgeType: "sale",
-    badgeText: "-30%",
-    image: "../assets/Syltherine.png"
-  },
-  {
-    id: 6,
-    name: "Leviosa",
-    subtitle: "Stylish cafe chair",
-    price: 2500000,
-    oldPrice: null,
-    badgeType: null,
-    badgeText: "",
-    image: "../assets/Leviosa.png"
-  },
-  {
-    id: 7,
-    name: "Lolito",
-    subtitle: "Luxury big sofa",
-    price: 7000000,
-    oldPrice: 14000000,
-    badgeType: "sale",
-    badgeText: "-50%",
-    image: "../assets/Lolito.png"
-  },
-  {
-    id: 8,
-    name: "Respira",
-    subtitle: "Outdoor bar table and stool",
-    price: 500000,
-    oldPrice: null,
-    badgeType: "new",
-    badgeText: "New",
-    image: "../assets/Respira.jpg"
-  },
-  {
-    id: 9,
-    name: "Syltherine",
-    subtitle: "Stylish cafe chair",
-    price: 2500000,
-    oldPrice: 3500000,
-    badgeType: "sale",
-    badgeText: "-30%",
-    image: "../assets/Syltherine.png"
-  },
-  {
-    id: 10,
-    name: "Leviosa",
-    subtitle: "Stylish cafe chair",
-    price: 2500000,
-    oldPrice: null,
-    badgeType: null,
-    badgeText: "",
-    image: "../assets/Leviosa.png"
-  },
-  {
-    id: 11,
-    name: "Lolito",
-    subtitle: "Luxury big sofa",
-    price: 7000000,
-    oldPrice: 14000000,
-    badgeType: "sale",
-    badgeText: "-50%",
-    image: "../assets/Lolito.png"
-  },
-  {
-    id: 12,
-    name: "Respira",
-    subtitle: "Outdoor bar table and stool",
-    price: 500000,
-    oldPrice: null,
-    badgeType: "new",
-    badgeText: "New",
-    image: "../assets/Respira.jpg"
-  },
-  {
-    id: 13,
-    name: "Syltherine",
-    subtitle: "Stylish cafe chair",
-    price: 2500000,
-    oldPrice: 3500000,
-    badgeType: "sale",
-    badgeText: "-30%",
-    image: "../assets/Syltherine.png"
-  },
-  {
-    id: 14,
-    name: "Leviosa",
-    subtitle: "Stylish cafe chair",
-    price: 2500000,
-    oldPrice: null,
-    badgeType: null,
-    badgeText: "",
-    image: "../assets/Leviosa.png"
-  },
-  {
-    id: 15,
-    name: "Lolito",
-    subtitle: "Luxury big sofa",
-    price: 7000000,
-    oldPrice: 14000000,
-    badgeType: "sale",
-    badgeText: "-50%",
-    image: "../assets/Lolito.png"
-  },
-  {
-    id: 16,
-    name: "Respira",
-    subtitle: "Outdoor bar table and stool",
-    price: 500000,
-    oldPrice: null,
-    badgeType: "new",
-    badgeText: "New",
-    image: "../assets/Respira.jpg"
-  }
-];
+
  
 const TOTAL_RESULTS = products.length;
  
@@ -222,7 +61,7 @@ function renderProducts() {
       }
  
       return `
-        <article class="product-card" data-name="${p.name}">
+        <article class="product-card" data-id="${p.id}" data-name="${p.name}">
           <div class="product-card__media">
             ${badgeHtml}
             <a href="product.html">
@@ -328,15 +167,6 @@ if (menuBtn && primaryNav) {
     primaryNav.classList.toggle("is-open");
   });
 }
- 
-document.addEventListener("click", (e) => {
-  const cartBtn = e.target.closest(".btn-cart");
-  if (cartBtn) {
-    const card = cartBtn.closest(".product-card");
-    const name = card && card.dataset && card.dataset.name ? card.dataset.name : "Product";
-    alert(`${name} added to cart`);
-  }
-});
- 
+
 renderProducts();
  
